578667fe42dadecd6b7c40f5fbdab0ad6a22df4a
kzhao <zhaokaijia@gmail.com>
2020-01-03 11:12:55 -0600

    init

--- compile_parallel.sh
+++ compile_parallel.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-mpicc -std=c99 -O3 parallel_sz_2.0.c -o parallel_sz_old -I/home/kazhao/z-checker-installer/SZ/sz-install/include /home/kazhao/z-checker-installer/SZ/sz-install/lib/libSZ.a /home/kazhao/z-checker-installer/SZ/sz-install/lib/libzlib.a -lm
+module purge
+module load gcc/8.2.0-g7hppkz
+module load openmpi
 
-mpicc -std=c99 -O3 parallel_zfp.c -o parallel_zfp -I/home/kazhao/z-checker-installer/zfp/include -I/home/kazhao/z-checker-installer/SZ/sz-install/include /home/kazhao/z-checker-installer/SZ/sz-install/lib/libSZ.a /home/kazhao/z-checker-installer/SZ/sz-install/lib/libzlib.a /home/kazhao/z-checker-installer/SZ/sz-install/lib/libzstd.a /home/kazhao/z-checker-installer/zfp/libzfp.a -lm
+mpicc -std=c99 -O3 parallel_sz_2.0.c -o parallel_sz -I/home/kazhao/z-checker-installer/SZ/sz-install/include /home/kazhao/z-checker-installer/SZ/sz-install/lib/libSZ.a /home/kazhao/z-checker-installer/SZ/sz-install/lib/libzlib.a /home/kazhao/zstd/lib64/libzstd.a -lm
 
-g++ -O3 -c sz_zfp_selector.cpp -o selector.o -I/home/xin/codes/zfp-0.3.1/inc -I/home/xin/utils/sz_master/include
+mpicc -std=c99 -O3 parallel_zfp.c -o parallel_zfp -I/home/kazhao/z-checker-installer/zfp/include -I/home/kazhao/z-checker-installer/SZ/sz-install/include /home/kazhao/z-checker-installer/SZ/sz-install/lib/libSZ.a /home/kazhao/z-checker-installer/SZ/sz-install/lib/libzlib.a /home/kazhao/z-checker-installer/SZ/sz-install/lib/libzstd.a /home/kazhao/z-checker-installer/zfp/lib/libzfp.a -lm
 
-mpicxx -O3 parallel_selector.c selector.o -o parallel_selector -I/home/xin/codes/zfp-0.3.1/inc -I/home/xin/utils/sz_master/include /home/xin/utils/sz_master/lib/libSZ.a /home/xin/utils/sz_master/lib/libzlib.a /home/xin/utils/sz_master/lib/libzstd.a /home/xin/codes/zfp-0.3.1/lib/libzfp.a
+g++ -O3 -c sz_zfp_selector.cpp -o selector.o -I/home/xin/codes/zfp-0.3.1/inc -I/home/kazhao/tools/SZ-2.0.3.0/install/include
+
+mpicxx -O3 parallel_selector.c selector.o -o parallel_selector -I/home/xin/codes/zfp-0.3.1/inc -I/home/kazhao/tools/SZ-2.0.3.0/install/include /home/kazhao/tools/SZ-2.0.3.0/install/lib/libSZ.a /home/kazhao/tools/SZ-2.0.3.0/install/lib/libzlib.a /home/kazhao/tools/SZ-2.0.3.0/install/lib/libzstd.a /home/xin/codes/zfp-0.3.1/lib/libzfp.a
 
 mpicc -std=c99 -O3 parallel_sz_kai.c -o parallel_sz_kai -I/home/kazhao/z-checker-installer/SZ/sz-install/include /home/kazhao/z-checker-installer/SZ/sz-install/lib/libSZ.a /home/kazhao/z-checker-installer/SZ/sz-install/lib/libzlib.a -lm
