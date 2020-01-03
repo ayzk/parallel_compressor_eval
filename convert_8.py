56fe0d910f37815a914c115319ee9eec4e956caf
kzhao <zhaokaijia@gmail.com>
2020-01-03 12:15:43 -0600

    init

--- convert_8.py
+++ convert_8.py
@@ -16,4 +16,5 @@ for dataset in ['/hurricane-100x500x500', 'miranda-256x384x384', 'qmcpack6k-3450
         r3 = r3 // 8 * 8
         data = data[:r1, :r2, :r3]
         print('{:} {:} from {:} to {:}'.format(dataset, filename, shape, "%d %d %d" % (r1, r2, r3)))
-        data.tofile("/lcrc/project/ECP-EZ/public/compression/datasets/0/{}".format(filename))
+        data.tofile("/lcrc/project/ECP-EZ/public/compression/datasets/0/{}_truncated.bin.dat".format(filename.split('.')[0]))
+        # print("/lcrc/project/ECP-EZ/public/compression/datasets/0/{}_truncated.bin.dat".format(filename.split('.')[0]))
