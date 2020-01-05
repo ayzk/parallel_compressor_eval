cfd0212b21c9b4312bf7380995a50adacee413a5
kzhao <zhaokaijia@gmail.com>
2020-01-04 23:14:36 -0500

    update

---
+++ convert_8_tardis.py
@@ -0,0 +1,20 @@
+import glob
+import numpy as np
+
+for dataset in ['/hurricane-100x500x500', 'miranda-256x384x384', 'qmcpack6k-34500x69x69']:
+    for filepath in glob.glob('/home/kzhao016/data/{:}/*'.format(dataset)):
+        filename = filepath.split('/')[-1]
+
+        data = np.fromfile(filepath, dtype=np.float32)
+        r1 = int(dataset.split('-')[-1].split('x')[0])
+        r2 = int(dataset.split('-')[-1].split('x')[1])
+        r3 = int(dataset.split('-')[-1].split('x')[2])
+        data = data.reshape([r1, r2, r3])
+        shape = "%d %d %d" % (r1, r2, r3)
+        r1 = r1 // 8 * 8
+        r2 = r2 // 8 * 8
+        r3 = r3 // 8 * 8
+        data = data[:r1, :r2, :r3]
+        print('{:} {:} from {:} to {:}'.format(dataset, filename, shape, "%d %d %d" % (r1, r2, r3)))
+        data.tofile("/lcrc/project/ECP-EZ/public/compression/datasets/0/{}_truncated.bin.dat".format(filename.split('.')[0]))
+        # print("/lcrc/project/ECP-EZ/public/compression/datasets/0/{}_truncated.bin.dat".format(filename.split('.')[0]))
